f54d8b30aaf9bef9074f0aafd9a39bdae175e998
Alexey <aleksey.konyshev@gmail.com>
2017-05-10 11:03:14 +0300

    Added legacy comments to all files

--- Calculator.h
+++ Calculator.h
@@ -1,6 +1,27 @@
-/**
- * 
- */
+////////////////////////////////////////////////////////////
+//
+// Calculator - Simple calculator realization by patterns
+// Copyright (C) 2017 - ? Alexey Konyshev (aleksey.konyshev@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
 #include "MathOpList.h"
 #include "DigitsList.h"
 #include "StrToDig.h"
@@ -59,28 +80,6 @@ public:
 	 */
 	void setScreen(Screen *newScrean);
 
-	/**
-	 * @brief Insert inputted symbol to the string which is screen
-	 * 
-	 * @param symbol
-	 */
-	//void addToScreen(std::string symbol);
-
-	/**
-	 * @brief Insert inputted double number to the string which is screen
-	 * 
-	 * @param number
-	 */
-	//void addToScreen(double number);
-
-	/**
-	 * @brief Get Screen, which could consist current view of result
-	 * 
-	 * @return screen string value
-	 */
-	//std::string getScreen();
-
-	//void screenClear();
 
 	MathOpList mathOpsList;
 	DigitsList digitsList;
@@ -97,5 +96,4 @@ public:
 	
 	double result;
 	
-//	std::stringstream screen;
 };
